788b8576b304ca88f6135a334c3969c0c8a75fa4
Aaron Housh <dispersias@gmail.com>
2026-07-28 14:22:57 -0700

    never pushed this to delete

--- src/plugins/combat/systems/find_target_system.rs
+++ src/plugins/combat/systems/find_target_system.rs
@@ -1,3 +1,19 @@
+use avian3d::physics_transform::Position;
 use bevy::prelude::*;
 
-pub fn find_target_system() {}
+use crate::plugins::components::{FindTarget, Target};
+
+pub fn find_target_system(
+    time: Res<Time>,
+    mut query: Query<(&Position, &mut FindTarget, &mut Target)>,
+) {
+    for (pos, mut find_target, mut target) in query.iter_mut() {
+        find_target.timer -= time.delta_secs();
+
+        if find_target.timer > 0. {
+            continue;
+        }
+
+        find_target.timer = find_target.timer_max;
+    }
+}
